3ae09e3085a843e7b98d60611493306e50de5f93
Zack Borges <zackartborges@gmail.com>
2021-10-04 13:35:24 -0400

    adds some refactoring to anagram.rb

--- ruby/anagram.rb
+++ ruby/anagram.rb
@@ -27,26 +27,57 @@
 
 # if it is all true, return true, if not return false
 
+# def anagram_checker(first_word, second_word)
+#   first_hash = {}
+#   second_hash = {}
+
+#   first_word.split("").each do |letter|
+#     if first_hash[letter] 
+#       first_hash[letter] += 1 #{s: 1}
+#     else
+#       first_hash[letter] = 1
+#     end
+#   end
+
+#   second_word.split("").each do |letter|
+#     if second_hash[letter] 
+#       second_hash[letter] += 1 #{s: 1}
+#     else
+#       second_hash[letter] = 1
+#     end
+#   end
+
+#   first_hash.each do |k, v|
+#     if second_hash[k] && second_hash[k] === v
+#     else
+#       return false
+#     end
+#   end
+# return true  
+# end
+
+
+# p anagram_checker("silent", "listen")
+# p anagram_checker("frog", "listen")
+
+# refactored:
 def anagram_checker(first_word, second_word)
-  first_hash = {}
-  second_hash = {}
-
-  first_word.split("").each do |letter|
-    if first_hash[letter] 
-      first_hash[letter] += 1 #{s: 1}
-    else
-      first_hash[letter] = 1
+ 
+  def hash_maker(word)
+    hash = {}
+    word.split("").each do |letter|
+      if hash[letter] 
+        hash[letter] += 1 #{s: 1}
+      else
+        hash[letter] = 1
+      end
     end
+    return hash
   end
 
-  second_word.split("").each do |letter|
-    if second_hash[letter] 
-      second_hash[letter] += 1 #{s: 1}
-    else
-      second_hash[letter] = 1
-    end
-  end
-
+  first_hash = hash_maker(first_word)
+  second_hash = hash_maker(second_word)
+  
   first_hash.each do |k, v|
     if second_hash[k] && second_hash[k] === v
     else
@@ -58,6 +89,7 @@ end
 
 p anagram_checker("silent", "listen")
 p anagram_checker("frog", "listen")
+p anagram_checker("hello!", "hello")
 
 # first {"s"=>1, "i"=>1, "l"=>1, "e"=>1, "n"=>1, "t"=>1}
 # second {"l"=>1, "i"=>1, "s"=>1, "t"=>1, "e"=>1, "n"=>1}
