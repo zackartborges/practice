dcf0ea39f8e8987717b0f8a79e31f4fe530f6086
Zack Borges <zackartborges@gmail.com>
2021-10-27 12:35:53 -0400

    adds flip_hsh problem

--- ruby/flip_hsh.rb
+++ ruby/flip_hsh.rb
@@ -2,22 +2,8 @@
 
 input= {"a" => 1, "b" => 2, "c" => 3}
 output= {1 => "a", 2 => "b", 3 => "c"}
-# while loop through hash inverting the hash
 
-# create new hash, 
-
-# loop through original 
-
-# set a blank variable 
-
-# set key equal to placeholder variable
-
-# set value equal to key 
-
-# set placeholder equal to value
-# map method to loop through array , grabbing k, v
-
-# input = {"a" => 1, "b" => 2, "c" => 3}
-p input.map {|k,v| p v,k}.to_h
-
+new_hash = {}
+input.map { |k,v | new_hash[v] = k}
+p new_hash
 # p new_hash
